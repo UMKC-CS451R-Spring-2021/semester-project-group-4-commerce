7b0ecd661f1a0d108393b969ab985179375c6387
Connor Bensyl <cicifire345@gmail.com>
2021-03-14 18:13:14 -0500

    push again

--- CommerceProject/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ CommerceProject/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -96,6 +96,13 @@ using Blazorise.Material;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 13 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\_Imports.razor"
+using System.Net.HttpClient;
+
+#line default
+#line hidden
+#nullable disable
     public partial class MainLayout : LayoutComponentBase
     {
         #pragma warning disable 1998
