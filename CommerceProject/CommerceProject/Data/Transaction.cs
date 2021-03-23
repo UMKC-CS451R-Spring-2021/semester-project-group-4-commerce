097e57d31b604e3ed2764f0c29fd2078c5a779fb
bkbui96 <47041543+bkbui96@users.noreply.github.com>
2021-03-22 21:10:48 -0500

    Added Transaction.cs & GetTransactionData, with working UI

---
+++ CommerceProject/CommerceProject/Data/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommerceProject.Data
+{
+    public class Transaction
+    {
+        private String location;
+        private String date;
+        private String money_gained;
+
+        public String getLocation() { return location; }
+        public String getDate() { return date; }
+        public String get_moneyGained() { return money_gained; }
+    }
+
+
+}
