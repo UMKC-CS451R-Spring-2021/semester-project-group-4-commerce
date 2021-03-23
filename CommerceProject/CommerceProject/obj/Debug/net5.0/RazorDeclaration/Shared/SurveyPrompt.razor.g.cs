fd9a42b020c8b656274bf6c2ed5afb9814c81160
bkbui96 <47041543+bkbui96@users.noreply.github.com>
2021-03-23 16:25:02 -0500

    Revamped UI, fits the Database information

--- CommerceProject/CommerceProject/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ CommerceProject/CommerceProject/obj/Debug/net5.0/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,77 +13,77 @@ namespace CommerceProject.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 1 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 2 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 3 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 4 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 5 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 6 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 7 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 8 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 9 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using CommerceProject;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 10 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using CommerceProject.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\_Imports.razor"
+#line 11 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\_Imports.razor"
 using Blazorise;
 
 #line default
@@ -97,7 +97,7 @@ using Blazorise;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "c:\users\khoii\source\repos\semester-project-group-4-commercelgs\commerceproject\CommerceProject\Shared\SurveyPrompt.razor"
+#line 12 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceWIP\CommerceProject\CommerceProject\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
