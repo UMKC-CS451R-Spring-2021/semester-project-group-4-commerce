571cec7f344829cbe9e4d4908a9a024ae4d23318
Connor Bensyl <cicifire345@gmail.com>
2021-03-23 19:59:13 -0500

    push before replacing with stuff from db

---
+++ CommerceProject/CommerceProject/Data/GetTransactionData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace CommerceProject.Data
+{
+    public class GetTransactionData
+    {
+        private String connectionString;
+
+        public GetTransactionData()
+        {
+            connectionString = "Data Source=NEWBLUE-PC\\COMMERCE_BANK;User ID=Developer2;Password=commerce1;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+
+        public IEnumerable<Transaction> GetTransactions()//will become relevant
+        {
+            string the_query = "SELECT Amount, Location, Processing_Date FROM TRANSACTIONS";//trying to figure out authentication
+            Connection.Open();
+            return Connection.Query<Transaction>(the_query).ToList<Transaction>();
+        }
+
+
+    }
+}
---
+++ CommerceProject/CommerceProject/Data/Transaction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace CommerceProject.Data
+{
+    public class Transaction
+    {
+        private String Location;
+        private String Processing_Date;
+        private float Amount;
+
+
+        public Transaction(float _Amount, String _Processing_Date, String _Location)
+        {
+            Amount = _Amount;
+            Processing_Date = _Processing_Date;
+            Location = _Location;
+        }
+
+        public Transaction()
+        {
+
+        }
+        public String getLocation()
+        {
+            return Location;
+        }
+        public String getDate()
+        {
+            return Processing_Date;
+        }
+        public float get_Amount()
+        {
+            return Amount;
+        }
+    }
+
+
+}
+
