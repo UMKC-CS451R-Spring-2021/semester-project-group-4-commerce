e3c6422e47099c427292e705f2557631bd07f698
Thomas-Tran-UMKC <ttnn3@mail.umkc.edu>
2021-03-24 01:22:32 -0500

    Add DataAccessLibrary

---
+++ DataAccessLibrary/PeopleData.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class PeopleData : IPeopleData
+    {
+        private readonly ISqlDataAccess _db;
+        public PeopleData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<PersonModel>> GetPeople()
+        {
+            string sql = "SELECT * FROM dbo.account_holder;";
+
+            return _db.LoadData<PersonModel, dynamic>(sql, new { });
+        }
+
+        public Task InsertPerson(PersonModel person)
+        {
+            string sql = @"insert into dbo.account_holder (First_Name, Last_Name, Email)
+                            values (@First_Name, @Last_Name, @Email);";
+
+            return _db.SaveData(sql, person);
+        }
+    }
+}
