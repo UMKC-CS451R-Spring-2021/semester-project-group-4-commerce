8be05a4101550d51b9589bdbd965964230caef58
Shelby Mohar <47090224+smgbx@users.noreply.github.com>
2021-04-13 07:53:08 -0500

    Added Brian V3.1 changes to UI

---
+++ CommerceProject/CommerceProject/Pages/Settings.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace CommerceProject.Pages
+{
+    public partial class Settings
+    {
+        public Settings()
+        {
+
+        }
+    }
+}
