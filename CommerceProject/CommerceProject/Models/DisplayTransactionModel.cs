40312545bf2c543a0a98b0b3f591c4a9cc72b5d0
Thomas-Tran-UMKC <ttnn3@mail.umkc.edu>
2021-04-26 05:12:43 -0500

    Update /addTrans
    Fix negative inputs
    Fix balance addition bug

--- CommerceProject/CommerceProject/Models/DisplayTransactionModel.cs
+++ CommerceProject/CommerceProject/Models/DisplayTransactionModel.cs
@@ -14,15 +14,19 @@ namespace CommerceProject.Models
         public String Processing_Date { get; set; }      // potential datatype problem
 
         public String Type { get; set; }
+
         [Required]
+        [Range(1, 10000, ErrorMessage = "Minimum value is $1.00. Maximum is $10,000")]
         public float Amount { get; set; }
+
         [Required]
         [StringLength(100, ErrorMessage = "100 characters or less.")]
         public String Description { get; set; }
 
         public float Balance { get; set; }
+
         [Required]
-        [MinLength(5, ErrorMessage = "Minimum 5 characters for Location.")]
+        [MinLength(4, ErrorMessage = "Minimum 4 characters for Location.")]
         public String Location { get; set; }
     }
 }
