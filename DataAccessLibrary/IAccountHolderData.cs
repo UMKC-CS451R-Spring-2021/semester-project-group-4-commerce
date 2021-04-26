35bc17377461e7c534f4157f59126d0d58cd50ba
Connor Bensyl <cicifire345@gmail.com>
2021-04-26 18:39:57 -0500

    changes

---
+++ DataAccessLibrary/IAccountHolderData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Models;
+namespace DataAccessLibrary
+{
+    public interface IAccountHolderData
+    {
+        
+        public Task ChangeName(AccountHolderModel account);
+
+    }
+}
