9f0db0a68598aced4817fa77e42956880436a588
bkbui96 <47041543+bkbui96@users.noreply.github.com>
2021-04-27 15:53:21 -0500

    Added navbar changes,

--- CommerceProject/CommerceProject/obj/Debug/net5.0/RazorDeclaration/Pages/TransactionsPage.razor.g.cs
+++ CommerceProject/CommerceProject/obj/Debug/net5.0/RazorDeclaration/Pages/TransactionsPage.razor.g.cs
@@ -13,92 +13,113 @@ namespace CommerceProject.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 1 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 2 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 3 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 4 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 5 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 6 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 7 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 8 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 9 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using CommerceProject;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 10 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using CommerceProject.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 11 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\_Imports.razor"
+#line 12 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\_Imports.razor"
 using Blazorise.Charts;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\Pages\TransactionsPage.razor"
-using CommerceProject.Data;
+#line 3 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\Pages\TransactionsPage.razor"
+using CommerceProject.Models;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 4 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\Pages\TransactionsPage.razor"
+using DataAccessLibrary;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 5 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\Pages\TransactionsPage.razor"
+using DataAccessLibrary.Models;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 6 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\Pages\TransactionsPage.razor"
+using Microsoft.AspNetCore.Http;
 
 #line default
 #line hidden
@@ -112,19 +133,89 @@ using CommerceProject.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 88 "C:\Users\Owner\Documents\GitHub\semester-project-group-4-commerce\CommerceProject\CommerceProject\Pages\TransactionsPage.razor"
+#line 143 "C:\Users\khoii\Source\Repos\semester-project-group-4-commerceFINAL3\CommerceProject\CommerceProject\Pages\TransactionsPage.razor"
        
-    List<Transaction> transaction_list = new List<Transaction>();
+    private List<PersonModel> people;
+    private List<TransactionModel> trans;
+    private List<TransactionModel> accountInfo;
+    private DisplayTransactionModel newTransaction = new DisplayTransactionModel();
+    private string depositValue = "CR";
+    private string withdrawalValue = "DR";
+
+    public string UserName;
 
     protected override async Task OnInitializedAsync()
     {
-        transaction_list = TransactionData.GetTransactions().ToList();
+        UserName = httpContextAccessor.HttpContext.User.Identity.Name; // gets current user's email
+
+        //people = await _db.GetPeople();
+
+        trans = await _trans.GetTransactions(UserName);
+        accountInfo = await _trans.GetAccountNum(UserName);
     }
+
+    private async Task InsertTrans()
+    {
+        DateTime now = DateTime.Now;
+        int accNum = accountInfo[0].Account_Num;
+
+
+        TransactionModel t = new TransactionModel
+        {
+            Account_Num = accNum,                   // 10010111 for trant@mail.com
+            Processing_Date = now.ToString(),
+            Type = chooseType(),
+            Amount = roundAmount(),
+            Description = newTransaction.Description,
+            Balance = newBalance(),
+            Location = newTransaction.Location
+        };
+
+        await _trans.InsertTransaction(t);              // add to database
+
+        trans.Add(t);                                   // add to list without refreshing page
+        newTransaction = new DisplayTransactionModel();     // wipe out form model
+    }
+
+    private float roundAmount()
+    {
+        double rounded = Math.Round(newTransaction.Amount, 2);
+        return (float)rounded;
+    }
+
+    private float newBalance()              // need to get balance from account? dont include in transaction? set to account
+    {
+        TransactionModel t = accountInfo[0];
+
+
+        double rounded = Math.Round(newTransaction.Amount, 2);
+        return (float)(t.Balance + rounded);
+    }
+
+    private string chooseType()
+    {
+
+
+        if (newTransaction.Type == "DR")
+        {
+            newTransaction.Amount *= -1;
+            return "DR";
+        }
+        else
+        {
+            return "CR";
+        }
+    }
+
+
+
 
 #line default
 #line hidden
 #nullable disable
-        [global::Microsoft.AspNetCore.Components.InjectAttribute] private GetTransactionData TransactionData { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private ITransactionData _trans { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IHttpContextAccessor httpContextAccessor { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IPeopleData _db { get; set; }
     }
 }
 #pragma warning restore 1591
