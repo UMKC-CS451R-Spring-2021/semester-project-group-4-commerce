a3cba504ba21e8dc817a71ef711b7571edae0bff
Thomas-Tran-UMKC <ttnn3@mail.umkc.edu>
2021-05-01 17:58:36 -0500

    Manual Merge from SqlTableDependency branch

--- DataAccessLibrary/INotificationListData.cs
+++ DataAccessLibrary/INotificationListData.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,7 +9,11 @@ namespace DataAccessLibrary
     {
         Task<List<NotificationListModel>> GetNotifications(string UserName);
         Task<List<NotificationListModel>> GetNotificationsUnread(string UserName);
-        Task SetReadToTrue(int Notification_ID);
+        Task<List<NotificationListModel>> GetNotificationsByDates(String UserName, string StartDate, string StopDate);
 
+        Task SetReadToTrue(int Notification_ID);
+        Task SetParameters(string UserName, bool timeIsOn, string beginTimeframe, string endTimeFrame,
+                                    bool WithdrawalIsOn, decimal withdrawalThresh, bool LowBalanceIsOn,
+                                    decimal balanceThresh, bool EmailNoteIsOn);
     }
 }
--- DataAccessLibrary/NotificationListData.cs
+++ DataAccessLibrary/NotificationListData.cs
@@ -24,23 +24,55 @@ namespace DataAccessLibrary
 
             return _db.LoadData<NotificationListModel, dynamic>(sql, new { });
         }
-
         public Task<List<NotificationListModel>> GetNotificationsUnread(String UserName)
         {
             string sql = @"SELECT * FROM notificationList
                             WHERE Account_Num = (SELECT Account_Num FROM account
 						                            INNER JOIN account_holder ON account.ID_Num = account_holder.ID_Num
 						                            WHERE account_holder.Email = '" + UserName + "')" +
-                            "AND [Read] = 0;";
+                                "AND [Read] = 0;";
 
             return _db.LoadData<NotificationListModel, dynamic>(sql, new { });
         }
+
+        public Task<List<NotificationListModel>> GetNotificationsByDates(String UserName, string StartDate, string StopDate)
+        {
+            string sql = @"SELECT * FROM notificationList
+                            WHERE Account_Num = (SELECT Account_Num FROM account
+						                            INNER JOIN account_holder ON account.ID_Num = account_holder.ID_Num
+						                            WHERE account_holder.Email = '" + UserName + "') " +
+                                "AND Date >= '" + StartDate + "' AND Date <= '" + StopDate + " 23:59:59.999';";
+
+            return _db.LoadData<NotificationListModel, dynamic>(sql, new { });
+        }
+
 
         public Task SetReadToTrue(int Notification_ID)
         {
             string sql = @"UPDATE notificationList
                             SET [Read] = 'true'
                             WHERE Notification_ID = " + Notification_ID + ";";
+
+            return _db.ExecuteSql(sql);
+        }
+
+        public Task SetParameters(string UserName, bool timeIsOn, string beginTimeframe, string endTimeFrame,
+                                    bool WithdrawalIsOn, decimal withdrawalThresh, bool LowBalanceIsOn,
+                                    decimal balanceThresh, bool EmailNoteIsOn)
+        {
+            string sql = @"	UPDATE notificationParameters
+	                            SET 
+	                            	Timeframe_Is_On = '" + timeIsOn + "'," + 
+	                            	" Begin_Time = cast('" + beginTimeframe + "' AS time)," +
+	                            	" End_Time = cast ('" + endTimeFrame + "' AS time)," +
+	                            	" Large_Withdrawal_Is_On = '" + WithdrawalIsOn + "'," +
+	                            	" Withdrawal_Amount = " + withdrawalThresh + "," +
+	                            	" Low_Balance_Is_On = '" + LowBalanceIsOn + "'," +
+	                            	" Low_Balance_Threshold = " + balanceThresh + "," +
+	                            	" Send_Email = '" + EmailNoteIsOn + "'" +
+	                            " WHERE Account_Num = (SELECT Account_Num FROM account" +
+	                            					" INNER JOIN account_holder ON account.ID_Num = account_holder.ID_Num" +
+	                            					" WHERE account_holder.Email = '" + UserName + "');";
 
             return _db.ExecuteSql(sql);
         }
