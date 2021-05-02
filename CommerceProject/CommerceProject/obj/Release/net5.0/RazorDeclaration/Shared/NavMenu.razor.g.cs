97e5998653a5e21cdf333b7a3e54e0cd82118abb
bkbui96 <47041543+bkbui96@users.noreply.github.com>
2021-05-01 19:14:38 -0500

    Added login page background Commerce logo redirects to homepage

--- CommerceProject/CommerceProject/obj/Release/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ CommerceProject/CommerceProject/obj/Release/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,85 +13,113 @@ namespace CommerceProject.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 1 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 2 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 3 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 4 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 5 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 6 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 7 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 8 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 9 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using CommerceProject;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 10 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using CommerceProject.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 11 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Blazorise;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\khoii\source\repos\semester-project-group-4-commercefinal1\CommerceProject\CommerceProject\_Imports.razor"
+#line 12 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\_Imports.razor"
 using Blazorise.Charts;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 1 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\Shared\NavMenu.razor"
+using CommerceProject.Services;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\Shared\NavMenu.razor"
+using DataAccessLibrary;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\Shared\NavMenu.razor"
+using DataAccessLibrary.Models;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 4 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\Shared\NavMenu.razor"
+using Microsoft.AspNetCore.Http;
 
 #line default
 #line hidden
@@ -103,6 +131,98 @@ using Blazorise.Charts;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 88 "C:\Users\khoii\Source\Repos\semester-project-group-4-commercecrunch\CommerceProject\CommerceProject\Shared\NavMenu.razor"
+       
+    private List<NotificationListModel> unreadNotifications;
+    public string UserName;
+    public int num_unread_notifs;
+
+
+    protected override void OnInitialized()
+    {
+        this.NotificationService.OnTriggeredNotification += this.TriggeredNotification;  //prepare to listen for new notifications
+
+    }
+
+
+    //populate bell notifications
+    protected override async Task OnInitializedAsync()
+    {
+
+        UserName = httpContextAccessor.HttpContext.User.Identity.Name; // gets current user's email
+
+        unreadNotifications = await _NotificationList.GetNotificationsUnread(UserName); // get current user's unread notifs
+
+        num_unread_notifs = unreadNotifications.Count;
+    }
+
+
+    public async void TriggeredNotification(NotificationListModel triggeredNotification)
+    {
+        unreadNotifications = await _NotificationList.GetNotificationsUnread(UserName); // get current user's unread notifs
+
+        num_unread_notifs = unreadNotifications.Count;
+
+        await InvokeAsync(() =>
+        {
+            base.StateHasChanged();
+        });
+    }
+
+    public string getNotificationType(NotificationListModel notification)
+    {
+        string not_type = "";
+
+        switch(notification.Notification_Type)
+        {
+            case 1:
+                not_type = "Timeframe Alert";
+                break;
+            case 2:
+                not_type = "Transaction Alert";
+                break;
+            case 3:
+                not_type = "Low Balance Alert";
+                break;
+            case 4:
+                not_type = "Overdraft Alert";
+                break;
+        }
+
+        return not_type;
+    }
+
+    public async void clearNewNotifications()
+    {
+        foreach (NotificationListModel curr_notification in unreadNotifications)
+        {
+            await _NotificationList.SetReadToTrue(curr_notification.Notification_ID);
+        }
+
+        unreadNotifications = await _NotificationList.GetNotificationsUnread(UserName); // get current user's unread notifs
+
+        num_unread_notifs = unreadNotifications.Count;
+
+        await InvokeAsync(() =>
+        {
+            base.StateHasChanged();
+        });
+    }
+
+
+    public void Dispose()
+    {
+        this.NotificationService.OnTriggeredNotification -= this.TriggeredNotification;
+    }
+
+
+#line default
+#line hidden
+#nullable disable
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private IHttpContextAccessor httpContextAccessor { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private INotificationListData _NotificationList { get; set; }
+        [global::Microsoft.AspNetCore.Components.InjectAttribute] private TriggeredNotificationBroadcastService NotificationService { get; set; }
     }
 }
 #pragma warning restore 1591
